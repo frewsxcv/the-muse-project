05d05b7467cedd21715291c845d7c839c10d053b
Corey Farwell <coreyf@rwell.org>
2016-11-14 19:57:40 -0500

    Utility function should itself return a promise.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var getJobs = function () {
   var url = 'https://api-v2.themuse.com/jobs?page=0';
-  fetch(url).then(res => {
+  return fetch(url).then(res => {
     return res.json();
-  }).then(json => {
-    console.log(json);
   });
 };
