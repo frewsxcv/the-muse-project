920f115a7d56b61d8653d7c9386a08032355b1b0
Corey Farwell <coreyf@rwell.org>
2016-11-14 19:57:58 -0500

    Add similar utility function for returning companies.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var getCompanies = function () {
+  var url = 'https://api-v2.themuse.com/companies?page=0';
+  return fetch(url).then(res => {
+    return res.json();
+  });
+};
+
 var getJobs = function () {
   var url = 'https://api-v2.themuse.com/jobs?page=0';
   return fetch(url).then(res => {
