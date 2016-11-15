601749d45c17624a8100d6e7b8258e75a66dc7ac
Corey Farwell <coreyf@rwell.org>
2016-11-14 20:51:20 -0500

    Add search button and job result fetching.

--- index.js
+++ index.js
@@ -16,11 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
   var companySelect = document.getElementById('company-select');
   console.assert(companySelect);
 
+  var searchBtn = document.getElementById('search-btn');
+  console.assert(searchBtn);
+
+  var jobResults = document.getElementById('job-results');
+  console.assert(jobResults);
+
   getCompanies().then(json => {
     for (let company of json.results) {
       var select = document.createElement('option');
       select.textContent = company.name;
       companySelect.appendChild(select);
     }
+  });
+
+  searchBtn.addEventListener('click', () => {
+    while (jobResults.hasChildNodes()) {
+      jobResults.lastChild.remove();
+    }
+    getJobs().then((json) => {
+      for (let job of json.results) {
+        var div = document.createElement('div');
+        div.textContent = job.name;
+        jobResults.appendChild(div);
+      }
+    });
   });
 }, false);
