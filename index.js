2620b9025d3405cf7847a99c06551bc0d89c00ba
Corey Farwell <coreyf@rwell.org>
2016-11-14 20:13:20 -0500

    Use `let` over `var`.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var getCompanies = function () {
-  var url = 'https://api-v2.themuse.com/companies?page=0';
+let getCompanies = function () {
+  let url = 'https://api-v2.themuse.com/companies?page=0';
   return fetch(url).then(res => {
     return res.json();
   });
 };
 
-var getJobs = function () {
-  var url = 'https://api-v2.themuse.com/jobs?page=0';
+let getJobs = function () {
+  let url = 'https://api-v2.themuse.com/jobs?page=0';
   return fetch(url).then(res => {
     return res.json();
   });
